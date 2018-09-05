bab8041f89534ee52efbb90c068b143762948cda
Victoria Brown <victoriabrown0710@gmail.com>
2018-09-05 12:54:44 -0500

    Initial commit

---
+++ express_lesson_one/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Da Best' });
+});
+
+module.exports = router;
